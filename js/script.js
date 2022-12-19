d2682d4f90324da9cc828f0d3c62eeeec7f5add8
robertvlad <vladrob96@gmail.com>
2022-12-19 09:54:53 +0100

    Bonus

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const {
 createApp({
     data(){
         return{
-            message: "Hello World"
+            message: "Hello World",
+            image: "https://i.picsum.photos/id/182/200/300.jpg?hmac=W6MnOpe7fP0LlNAyWl6rzWbjyLOM3ix2TXRcFx7vEPE"
         }
     }
 }).mount("#app")
